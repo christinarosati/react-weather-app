50c031dca85e068748956346bc61208898fa67ac
christinarosati <chrisrosati22@gmail.com>
2022-10-27 12:04:25 -0400

    Added basic forecast component

---
+++ src/WeatherForecast.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thur</div>
+          <div className="WeatherForecast-icon">
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+              width="42"
+            />
+          </div>
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">20°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
